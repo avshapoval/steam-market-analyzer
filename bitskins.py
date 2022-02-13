8a06b01f3b846e8e0af24145e4c3a4889035d4d5
avshapoval <96253415+avshapoval@users.noreply.github.com>
2022-02-13 18:55:52 +0300

    Add files via upload

---
+++ bitskins.py
@@ -0,0 +1,148 @@
+import os
+import sys
+
+from funcs import *
+
+
+my_secret = 'H3PFLIKG4ERJKXJY'
+api = "8f384375-5750-4664-9731-692ae4e47701"
+zenscrape_headers = {"apikey": "6c3c9f90-73b0-11ec-ab8e-1728271e8221"}
+
+min_price = 0
+max_price = float('inf')
+min_profit_percent = 0.4
+filename = "bitskins.csv"
+
+found = []
+result = []
+print_help()
+while True:
+    prompt = "Please input command(8 to get help, 0 to exit program): "
+    global_command = get_command_int(prompt)
+    print()
+    # Search items with given price range and profit rate on bitskins
+    if global_command == 1:
+        found = []
+        result = []
+        token = get_token(my_secret)
+
+        url = f"https://bitskins.com/api/v1/get_all_item_prices/?api_key={api}&code={get_token(my_secret)}" \
+              f"&app_id=730"
+        basic_prices = {
+              item['market_hash_name']: float(item['price']) for item in requests.get(url).json()["prices"]
+        }
+
+        url2 = f"https://bitskins.com/api/v1/get_price_data_for_items_on_sale/?api_key={api}" \
+               f"&code={get_token(my_secret)}&app_id=730"
+        selling_prices = [
+            [item['market_hash_name'], float(item['lowest_price'])] for item in requests.get(url2).json()["data"]["items"]
+            if item["recent_sales_info"] is not None and float(item['lowest_price']) != 0
+        ]
+        
+
+        for item in selling_prices:
+            if not (min_price <= item[1] <= max_price):
+                continue
+            if (min_profit_percent + 1) * item[1] <= basic_prices[item[0]]:
+                found.append([
+                    item[0], item[1], basic_prices[item[0]],
+                    (basic_prices[item[0]]/item[1] - 1)*100,
+                    str(1 - (basic_prices[item[0]] - item[1])/basic_prices[item[0]]) + "%"
+                ])
+
+        found.sort(key=lambda item: item[-2], reverse=True)
+
+    # Get info for found items on steam(sometimes, bitskins is not precise)
+    elif global_command == 2:
+        prompt = "Note that the process of checking prices is relatively long because every single item" \
+                 "is checked directly into steam.\n" \
+                 "Also note that stopping running process will lead to loss of every checked item info." \
+                 "Zenscrape is used to avoid temporary IP ban.\n" \
+                 "Please enter the amount of items to check price(-1 if all): "
+        items_to_check = get_command_int(prompt)
+        if not found:
+            print("You haven't searched for any items or none of them were found.\n")
+            continue
+
+        if items_to_check == -1:
+            items_to_check = len(found)
+        elif items_to_check <= 0:
+            continue
+
+        result = []
+        for index, item in enumerate(found[:items_to_check]):
+            url = f"https://steamcommunity.com/market/listings/730/{item[0]}"
+            print(f"Checking steam data of {index + 1} item of {items_to_check}, url: {url}")
+
+            try:
+                median_price = find_median_steam_price(url, zenscrape_headers)
+            except AttributeError:
+                print("Failed")
+                continue
+
+            print("Succesful")
+            item.extend([median_price, (median_price / item[1] - 1) * 100,
+                         str((1 - (median_price - item[1]) / median_price) * 100) + "%"])
+            result.append(item)
+
+    # Change price range
+    elif global_command == 3:
+        while True:
+            prompt = "Please input minimum price on Bitskins: "
+            min_price = get_price(prompt)
+            break
+        while True:
+            prompt = "Please input maximum price on Bitskins: "
+            max_price = get_price(prompt)
+            if max_price < min_price or max_price < 0:
+                continue
+            break
+        print("Price range saved\n")
+
+    # Change minimum profit
+    elif global_command == 4:
+        min_profit_percent = get_profit_percent() / 100
+
+    # Change filename for future data saving
+    elif global_command == 5:
+        filename = input("Please input filename without extension: ") + ".csv"
+
+    # Save data
+    elif global_command == 6:
+        if not found:
+            print("Saved data is empty, nothing is written")
+        try:
+            os.mkdir("results")
+        except FileExistsError:
+            pass
+
+        prompt = "Which data to save: checked in steam or raw bitskins data?(2 - steam, 1 - bitskins): "
+        user_command = get_command_int(prompt)
+        if user_command == 0:
+            continue
+
+        elif user_command == 1:
+            prompt = "Should technical info be saved? y/n"
+            tech_save = input(prompt)
+            if tech_save == "y":
+                save_data_bitskins(filename, True, found)
+            else:
+                save_data_bitskins(filename, False, found)
+
+        elif user_command == 2:
+            prompt = "Should technical info be saved? y/n: "
+            tech_save = input(prompt)
+            if tech_save == "y":
+                save_data_steam(filename, True, result)
+            else:
+                save_data_steam(filename, False, result)
+        print("Data saved succesfully\n")
+
+    # Output help "page"
+    elif global_command == 8:
+        print_help()
+        continue
+
+    # Exit program
+    elif global_command == 0:
+        sys.exit()
---
+++ funcs.py
@@ -0,0 +1,118 @@
+import json
+import re
+import requests
+from statistics import median
+import pyotp
+import csv
+
+
+def get_token(secret):
+    """Get current token for given secret code"""
+    my_token = pyotp.TOTP(secret)
+    token = my_token.now()
+    return token
+
+
+def get_profit_percent():
+    """Get minimum profit percent from user"""
+    while True:
+        try:
+            percent = float(input("Введите минимальный интересующий вас процент скидки по сравнению с "
+                                  "ценой в steam в формате целого или дробного числа от 0 до 100: "))
+        except ValueError:
+            print("Вы ввели неправильное число")
+        else:
+            if percent < 0 or percent > 100:
+                print("Вы ввели неправильное число")
+            print("Процент сохранен\n")
+            return percent
+
+
+def print_help():
+    """Print help page to console"""
+    print("Please input:\n"
+          "1, to start searching of skins in given price range and profit rate on bitskins.com\n"
+          "2, to get data for found items in steam(actual data and more precise than on bitskins.com)\n"
+          "3, to change price range for searching\n"
+          "4, to change minimum profit rate\n"
+          "5, to change filename for saving results\n"
+          "6, to save data into file\n"
+          "8, to get this text\n"
+          "0, to exit program\n")
+
+
+def find_median_steam_price(url:str, headers, length=10):
+    """Get median steam price using Zenscape API for last *length* deals"""
+    if "StatTrak" in url:
+        url = url.replace("StatTrak™ ", "StatTrak%E2%84%A2%20")
+    params = (
+        ("url", url),
+    )
+    rs = requests.get('https://app.zenscrape.com/api/v1/get', headers=headers, params=params)
+    m = re.search(r'var line1=(.+);', rs.text)
+    data_str = m.group(1)
+    data = json.loads(data_str)
+    data = data[-length:] if len(data) >= length else data
+    median_price = median([float(sell[1]) for sell in data])
+    return median_price
+
+
+def get_command_int(prompt: str):
+    """Safely get command from user"""
+    while True:
+        try:
+            command = int(input(prompt))
+        except ValueError:
+            print("You did not input integer, please try again")
+        else:
+            break
+    return command
+
+
+def save_data_steam(filename: str, tech: bool, data):
+    """Save data icluding steam prices to .csv file"""
+    with open("results/" + filename, 'w', newline='', encoding='utf-16') as f:
+        bit_writer = csv.writer(f)
+        if tech:
+            bit_writer.writerow([
+                "item_name", "item_lowest_price", "item_base_price", "bit_based_profit_rate",
+                "bit_based_discount", "item_steam_price", "real_profit_rate", "real_discount"
+            ])
+            bit_writer.writerows(data)
+        else:
+            bit_writer.writerow([
+                "item_name", "item_lowest_price", "real_profit_rate"
+            ])
+            for item in data:
+                bit_writer.writerow([item[0], item[1], item[6]])
+        print("Data saved\n")
+
+
+def save_data_bitskins(filename: str, tech: bool, data):
+    """Save data from Bitskins only to .csv file """
+    with open("results/" + filename, 'w', newline='', encoding='utf-16') as f:
+        bit_writer = csv.writer(f)
+        if tech:
+            bit_writer.writerow([
+                "item_name", "item_lowest_price", "item_base_price",
+                "bit_based_profit_rate", "bit_based_discount"
+            ])
+            bit_writer.writerows(data)
+        else:
+            bit_writer.writerow([
+                "item_name", "item_lowest_price", "bit_based_profit_rate"
+            ])
+            for item in data:
+                bit_writer.writerow([item[0], item[1], item[3]])
+        print("Data saved\n")
+
+
+def get_price(prompt: str):
+    """Safely get price range from user"""
+    while True:
+        try:
+            min_max_price = float(input(prompt))
+        except ValueError:
+            print("Число введено неверно, попробуйте еще раз")
+        else:
+            return min_max_price
